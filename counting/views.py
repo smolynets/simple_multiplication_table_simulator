a7f7a76154d123cba73744eeddfe606557caec12
Oleh Smolynets <smolynets@gmail.com>
2020-12-04 22:42:11 +0200

    fix template

--- counting/views.py
+++ counting/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def index(request):
     if request.method == "POST":
         choice = request.POST["choice"]
-    return render(request, "index.html", {"one": 2, "two": 4})
+        import ipdb
+
+        ipdb.set_trace()
+    return render(request, "index.html", {"one": 2, "two": 4, "third": 5, "four": 7})
