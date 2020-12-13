61c2b909568144c859c61750be361a49ddd79cd2
Oleh Smolynets <smolynets@gmail.com>
2020-12-13 18:43:23 +0200

    feat: added session with how_many_seconds_until_midnight()

--- counting/views.py
+++ counting/views.py
@@ -1,9 +1,28 @@
 import random
+from datetime import datetime, timedelta
 
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
+def how_many_seconds_until_midnight():
+    """Get the number of seconds until midnight."""
+    tomorrow = datetime.now() + timedelta(1)
+    midnight = datetime(
+        year=tomorrow.year, month=tomorrow.month, day=tomorrow.day, hour=0, minute=0, second=0
+    )
+    return (midnight - datetime.now()).seconds
+
+
+@csrf_exempt
 def index(request):
+    seconds_until_midnight = how_many_seconds_until_midnight()
+    if not request.session.get("aswers"):
+        request.session["aswers"] = 0
+        request.session.set_expiry(seconds_until_midnight)
+    request.session["aswers"] += 1
+    if request.session["aswers"] >= 10:
+        return render(request, "limit_per_day.html", {},)
     if request.method == "POST":
         choice = int(request.POST["choice"])
         form_multiplication_result = int(request.POST["result"])
