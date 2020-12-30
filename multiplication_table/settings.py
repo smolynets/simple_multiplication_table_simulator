c697788e35d8f9893d5006d11ee64cf47f1d5341
Oleh Smolynets <smolynets@gmail.com>
2020-12-30 23:41:54 +0200

    added STATIC_ROOT variable to settings

--- multiplication_table/settings.py
+++ multiplication_table/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from .external_settings import debug, time_zone
@@ -114,3 +115,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
