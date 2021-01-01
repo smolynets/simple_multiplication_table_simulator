68bbd2af4ec8872d160a382bf724dd24de5b4870
Oleh Smolynets <smolynets@gmail.com>
2021-01-01 13:49:58 +0200

    feat: added singleton witn greeting_text field

--- counting/admin.py
+++ counting/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from counting.models import FinishPerDay
+from counting.models import FinishPerDay, SiteConfiguration
+from solo.admin import SingletonModelAdmin
 
 
 class FinishPerDayAdmin(admin.ModelAdmin):
     list_display = ("finish_time", "score", "spended_time")
 
 
+config = SiteConfiguration.get_solo()
+
+
 admin.site.register(FinishPerDay, FinishPerDayAdmin)
+admin.site.register(SiteConfiguration, SingletonModelAdmin)
--- counting/models.py
+++ counting/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from solo.models import SingletonModel
 
 
 class FinishPerDay(models.Model):
     finish_time = models.CharField(max_length=50)
     score = models.CharField(max_length=10)
     spended_time = models.CharField(max_length=1000)
+
+
+class SiteConfiguration(SingletonModel):
+    greeting_text = models.TextField()
+
+    def __str__(self):
+        return "Site Configuration"
+
+    class Meta:
+        verbose_name = "Site Configuration"
