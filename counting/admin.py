230df764041623f1f681e1955317e1ad1cb63fcc
Oleh Smolynets <smolynets@gmail.com>
2021-01-01 12:41:48 +0200

    fix: display all fileds in admin

--- counting/admin.py
+++ counting/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from counting.models import FinishPerDay
 
-admin.site.register(FinishPerDay)
+
+class FinishPerDayAdmin(admin.ModelAdmin):
+    list_display = ("finish_time", "score", "spended_time")
+
+
+admin.site.register(FinishPerDay, FinishPerDayAdmin)
